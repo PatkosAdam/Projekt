6c72cf50d2d95bed497903ba8cdd24593c5d2272
Ivan <patkosadi11@freemail.hu>
2024-09-03 11:05:05 +0200

    appending with variables

--- asd.py
+++ asd.py
@@ -1,11 +1,22 @@
-# importing the module
+# imports
 import pandas as pd
+from datetime import date
 
 # importing summary xlsx file
 df2 = pd.read_excel('sum.xlsx')
 
+# variables
+mv_torzsszam = 123
+mv_nev = 'nev'
+koltseghely = 123
+megnevezes = '???????'
+kezdet = date(2020, 5, 17)
+veg = date(2020, 5, 20)
+napok = 123
+
+
 # append new row
-df2.loc[len(df2.index)] = [900, 'TAA', 900, 'RAAAA', 900, 900, 900] # new values
+df2.loc[len(df2.index)] = [mv_torzsszam, mv_nev, koltseghely, megnevezes, kezdet, veg, napok] # new values
 df2.set_index('Munkavállaló törzsszáma', inplace=True) # reassigning index row
 df2.to_excel('sum.xlsx') # saving
 print('DataFrame is written to Excel File successfully.') # successful message
