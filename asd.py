9bb81cbc415e1031b6d65e11c54a9435f571b5db
Ivan <patkosadi11@freemail.hu>
2024-09-03 11:24:37 +0200

    date formatting is right

--- asd.py
+++ asd.py
@@ -1,22 +1,28 @@
 # imports
 import pandas as pd
 from datetime import date
+import xlsxwriter
 
 # importing summary xlsx file
-df2 = pd.read_excel('sum.xlsx')
+df = pd.read_excel('sum.xlsx')
 
 # variables
-mv_torzsszam = 123
-mv_nev = 'nev'
-koltseghely = 123
-megnevezes = '???????'
-kezdet = date(2020, 5, 17)
-veg = date(2020, 5, 20)
-napok = 123
-
+mv_torzsszam = 90
+mv_nev = 'Bela'
+koltseghely = 10
+megnevezes = 're'
+kezdet = date(1960, 5, 17)
+veg = date(2000, 5, 20)
+napok = 9
 
 # append new row
-df2.loc[len(df2.index)] = [mv_torzsszam, mv_nev, koltseghely, megnevezes, kezdet, veg, napok] # new values
-df2.set_index('Munkavállaló törzsszáma', inplace=True) # reassigning index row
-df2.to_excel('sum.xlsx') # saving
+df.loc[len(df.index)] = [mv_torzsszam, mv_nev, koltseghely, megnevezes, kezdet, veg, napok] # new values
+df.set_index('Munkavállaló törzsszáma', inplace=True) # reassigning index row
+
+# formatting
+df["Vége"] = pd.to_datetime(df["Vége"]).dt.strftime("%Y-%m-%d")
+df["Kezdete"] = pd.to_datetime(df["Kezdete"]).dt.strftime("%Y-%m-%d")
+
+# saving
+df.to_excel('sum.xlsx') # saving
 print('DataFrame is written to Excel File successfully.') # successful message
