ee289a1d1006d971c6498f8dcbba95b4305e9c0a
BaloghBarnabas1 <b.barnus.2004@gmail.com>
2024-09-03 11:57:34 +0200

    kommentelések

--- asd2.py
+++ asd2.py
@@ -10,13 +10,13 @@ pytesseract.pytesseract.tesseract_cmd = './Tesseract-OCR/tesseract'
 #print(pytesseract.image_to_string(Image.open("angol.png")))
 #print(pytesseract.image_to_string(Image.open('tabla.png')))
 
-images = convert_from_path("valami.pdf", poppler_path= "site-packages\\poppler-24.07.0\\Library\\bin")
+images = convert_from_path("valami.pdf", poppler_path= "site-packages\\poppler-24.07.0\\Library\\bin") # PDF képpé konvertálása
 
-for i in range(len(images)):
-  # Save pages as images in the pdf
+for i in range(len(images)): 
+  # Minden PDF oldal egy külön kép lesz
   images[i].save('page'+ str(i) +'.jpg', 'JPEG')
 
-print(pytesseract.image_to_string(Image.open("page9.jpg"), lang='hun'))
+print(pytesseract.image_to_string(Image.open("page9.jpg"), lang='hun')) # teszt hogy megfelelően beolvassa-e a program a táblázatokat
 #b =print(pytesseract.image_to_string(Image.open("page10.jpg"), lang='hun'))
 #c =print(pytesseract.image_to_string(Image.open("page11.jpg"), lang='hun'))
 #d =print(pytesseract.image_to_string(Image.open("page12.jpg"), lang='hun'))
